3e14d25b247e539dca6f3bee4536eb8fa0e71515
Justin Lloyd <justin@justinlloyd.io>
2022-07-25 21:52:13 -0700

    Add logic to handle screen initialization for either workstation or Raspberry Pi

--- src/main.py
+++ src/main.py
@@ -12,16 +12,23 @@ def wait():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == KEYDOWN and event.key == K_f:
-                return
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
 
 
 os.putenv('SDL_FBDEV', '/dev/fb0')
 pygame.init()
 
-lcd = pygame.display.set_mode((320, 200))
+displayinfo = pygame.display.Info()
+if (displayinfo.current_w==1100):
+    lcd = pygame.display.set_mode((1100, 3840))
+else:
+    lcd = pygame.display.set_mode((550, 1920))
+
 lcd.fill((255, 0, 0))
 pygame.display.update()
+wait()
 pygame.display.quit()
 pygame.quit()
 exit()
